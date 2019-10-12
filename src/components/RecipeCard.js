c4075935f4ee3f4e5bbc01c5763308d16ed2ce57
Bilal Sevinc <bilalresmi@gmail.com>
2019-10-12 23:57:45 +0300

    Updated

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -4,14 +4,15 @@ import { Link } from 'react-router-dom';
 const RecipeCard  = ({key, title, calories, image, ingredients, source, details}) => {
     return(
         
-        <div className="col-md-6" key={key}>
-            <Link to="/recipe">
-                <h1>{title}</h1>
-            </Link>  
-            <Link to="/recipe">  
-                <img src={image} alt=""/>
-                
-            </Link>
+        <div className="col-md-3 mb-2" key={key}>
+            <div className="card">
+                <Link to="/recipe" className="recipe-card">
+                    <img className="card-img-top" src={image} alt=""/>
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5> 
+                    </div>
+                </Link>
+            </div>
         </div>
         
     );
