855acafb7502d9782e2bf168aa1e8d6238f37eb7
bilalsevis <bilalresmi@gmail.com>
2019-10-16 17:48:00 +0300

    Updates

---
+++ precache-manifest.32c2e66afd4140b3a3b1d6eb837c49ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "995b0f5ba7dddf517626b9226fd8f750",
+    "url": "/Recipe-Search-App/index.html"
+  },
+  {
+    "revision": "95e5fcf92b0708f2c5e8",
+    "url": "/Recipe-Search-App/static/css/main.7b9d3760.chunk.css"
+  },
+  {
+    "revision": "892c70c496f237636f31",
+    "url": "/Recipe-Search-App/static/js/2.0decba57.chunk.js"
+  },
+  {
+    "revision": "95e5fcf92b0708f2c5e8",
+    "url": "/Recipe-Search-App/static/js/main.169d8e73.chunk.js"
+  },
+  {
+    "revision": "9bdc910a85d86f2c06c6",
+    "url": "/Recipe-Search-App/static/js/runtime-main.376adeb0.js"
+  }
+]);
